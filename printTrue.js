a7cb45fd83f25e26791de97ba265c21d5dd33ed0
Srikrishna <krishna333sp@gmail.com>
2023-03-22 11:37:34 +0530

    program

--- printTrue.js
+++ printTrue.js
@@ -2,7 +2,14 @@
 // If a number is divisible by 7, then print World
 // If a number is divisible by both 5 and 7, then print Hello World
 
+//using else if
 let num = 70;
 if(num%5 === 0 && num%7 === 0) console.log("Hello World");
 else if(num%5 === 0) console.log("Hello");
 else if(num%7 === 0) console.log("World");
+
+//or with conditional operator
+num%5 === 0 && num%7 === 0 ? 
+console.log('Hello World') : num%5 === 0 ? 
+console.log("Hello") : num%7 === 0 ? 
+console.log('World') : "Invalid Entry";
